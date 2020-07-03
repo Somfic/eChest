9908eb07fed2ae44682f6594a0248ca7a29d8429
Somfic <somfic@gmail.com>
2020-07-03 14:06:49 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php require_once './core/init.php'; ?>
-<?php include './includes/header.php'; ?>
+<?php include './includes/top.php'; ?>
 
 <?php
 if (Input::exists()) {
@@ -27,7 +27,7 @@ if (Input::exists()) {
             $user = new User();
             try {
                 $user->register(Input::get('username'), Input::get('password'));
-                Result::success('Account created', 'Your account has succesfully been created.', 'index.php');
+                Result::success('Account created', 'Your account has succesfully been created', 'index.php');
             } catch (Exception $ex) {
                 Result::error('Account could not be created', $ex->getMessage(), '/register.php');
             }
@@ -40,25 +40,35 @@ if (Input::exists()) {
 }
 ?>
 
-<form action="" method="POST">
-    <div class="form-input floating">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')) ?>" autocomplete="off">
+<div class="row height-screen">
+    <div class="col-12 col-lp-6 flex center bg-contrast">
+        <div class="align-center width-full py-2">
+            <h1><b>eChest</b></h1>
+            <h3>Create a new account</h3>
+        </div>
     </div>
-    <div class="form-input floating">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" value="" autocomplete="off">
+    <div class="col-12 col-lp-6 flex center px-4">
+        <form action="" method="POST">
+            <div class="form-input floating">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')) ?>" autocomplete="off">
+            </div>
+            <div class="form-input floating">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" value="" autocomplete="off">
+            </div>
+
+            <div class="form-input floating">
+                <label for="password_repeat">Password confirm</label>
+                <input type="password" name="password_repeat" id="password_repeat" value="" autocomplete="off">
+            </div>
+            <input type="hidden" name="token" value="<?php echo Token::generate() ?>">
+            <input class="button primary width-full mb-2 mt-3" type="submit" value="Register">
+        </form>
+        <div class="align-center mt-2 flex-vertical">
+            <a href="/login.php">I already have an account</a>
+        </div>
     </div>
+</div>
 
-    <div class="form-input floating">
-        <label for="password_repeat">Password confirm</label>
-        <input type="password" name="password_repeat" id="password_repeat" value="" autocomplete="off">
-    </div>
-    </div>
-
-    <input class="button" type="submit" value="Register">
-
-    <input type="hidden" name="token" value="<?php echo Token::generate() ?>">
-</form>
-
-<?php include './includes/footer.php'; ?>
+<?php include './includes/bottom.php'; ?>
