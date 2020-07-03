247335db0e58bb593bc9b13313575977419396ac
Somfic <somfic@gmail.com>
2020-07-03 14:06:52 +0200

    Update result.php

--- result.php
+++ result.php
@@ -1,16 +1,35 @@
 <?php require_once './core/init.php'; ?>
-<?php include './includes/header.php'; ?>
+<?php include './includes/top.php'; ?>
 
-<div class="pullout">
-    <h2 class="pullout-header">
-        <?php echo Session::get('result-title'); ?>
-    </h2>
+<?php header("refresh:4;url=" . Session::get('result-redirect'));
+?>
 
-    <p class="pullout-paragraph">
-        <?php echo Session::get('result-message'); ?>
-    </p>
+<div class="flex center height-screen align-center">
+    <div>
+        <h1 class="mb-2">
+            <?php if (Session::get('result-type') == 'success') : ?>
+                <i class="fas fa-check-circle success"></i>
+            <?php else : ?>
+                <i class="fas fa-times-circle error"></i>
+            <?php endif; ?>
+        </h1>
+        <h1>
+            <?php echo Session::get('result-title'); ?>
+        </h1>
+
+        <h2 class="muted mb-2">
+            <?php echo Session::get('result-message'); ?>
+        </h2>
+        <div class="mb-2">
+            <div class="waiter">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <p class="m-a">You will be <a href="<?php echo Session::get('result-redirect'); ?>">redirected</a> in just a moment</p>
+    </div>
 </div>
 
-<a href="<?php echo Session::get('result-redirect'); ?>">Redirect</a>
-
-<?php include './includes/footer.php'; ?>
+<?php include './includes/bottom.php'; ?>
