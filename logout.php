682ae9fa0051ada8e024c9f20affd5aefdb4d648
Somfic <somfic@gmail.com>
2020-07-03 14:06:44 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,4 +4,4 @@ require_once './core/init.php';
 $user = new User();
 $user->logout();
 
-Result::success('You were logged out', 'You were succesfully logged out', 'index.php');
+Result::success('You were logged out', 'See you soon!', 'index.php');
