f5aa8aebf9b1a05a69e2fe619197a64154c3b78a
Somfic <somfic@gmail.com>
2020-07-03 14:06:40 +0200

    Added styling

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php require_once './core/init.php'; ?>
-<?php include './includes/header.php'; ?>
+<?php $title = "eChest - Login"; ?>
+<?php include './includes/top.php'; ?>
 
 <?php
 if (Token::check(Input::get('token'))) {
@@ -17,9 +18,9 @@ if (Token::check(Input::get('token'))) {
         $login = $user->login(Input::get('username'), Input::get('password'), $remember);
 
         if ($login) {
-            Result::success('Logged in', 'You are succesfully logged in!', 'index.php');
+            Result::success('Logged in', greeting() . ' ' . Input::get('username') . '!', 'index.php');
         } else {
-            Result::success('You weren\'t logged in', 'You could not be succesfully logged in', 'index.php');
+            Result::error('You weren\'t logged in', 'Hmm, let\'s try that again...', 'index.php');
         }
     } else {
         foreach ($validation->errors() as $error) {
@@ -28,25 +29,38 @@ if (Token::check(Input::get('token'))) {
     }
 }
 ?>
+<div class="row height-screen">
+    <div class="col-12 col-lp-6 flex center bg-contrast">
+        <div class="align-center width-full py-2">
+            <h1><b>eChest</b></h1>
+            <h3>Please log in to continue</h3>
+        </div>
+    </div>
+    <div class="col-12 col-lp-6 flex center px-4">
+        <form action="" method="POST">
+            <div class="form-input floating">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" required>
+            </div>
+            <div class="form-input floating">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <div class="form-input">
+                <label for="remember">
+                    <input type="checkbox" name="remember" id="remember">
+                    <span class="check-text">Remember me</span>
+                </label>
+            </div>
 
-<h2>Login</h2>
-<form action="" method="POST">
-    <div class="form-input floating">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" autocomplete="off">
-    </div>
-    <div class="form-input floating">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" autocomplete="off">
-    </div>
-    <div class="form-input">
-        <label for="remember">
-            <input type="checkbox" name="remember" id="remember">
-            <span class="check-text">Remember me</span>
-        </label>
-    </div>
+            <input type="hidden" name="token" value="<?php echo Token::generate() ?>">
 
-    <input type="hidden" name="token" value="<?php echo Token::generate() ?>">
-    <input class="button" type="submit" value="Log in">
-</form>
-<?php include './includes/footer.php'; ?>
+            <input class="button primary width-full mb-2 mt-3" type="submit" value="Log in">
+        </form>
+        <div class="align-center mt-2 flex-vertical">
+            <a href="/register.php">Register an account</a>
+            <a href="#">Forgot your password?</a>
+        </div>
+    </div>
+</div>
+<?php include './includes/bottom.php'; ?>
